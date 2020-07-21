9ee0e6a151b93d254287bf90f8a9c915cf3b3e7a
cswil <56652842+cswil@users.noreply.github.com>
2020-07-21 11:26:56 +0100

    added member join event

--- cogs/event_cog.py
+++ cogs/event_cog.py
@@ -9,5 +9,8 @@ class event_cogs(commands.Cog):
     async def on_ready(self):
         print('The bot is up and running')  
 
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        await member.send('Hey there! Welcome to `learn python discord`. Courses are every tuesday, in between these days we have challenges running, which you can see in the #challenges channel. We have a custom bot (this one) us `lp!help` to see what you can do')
 def setup(bot):
     bot.add_cog(event_cogs(bot))
